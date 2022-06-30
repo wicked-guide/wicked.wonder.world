e1e4622280034be18fbcfc231a8070c137f0e919
wicked <wicked.wonder.world@gmail.com>
2022-06-30 09:21:48 +0900

    プロジェクト作成

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'たいとる',
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <div>index.vue</div>
+  </section>
+</template>
+
+<script>
+export default {
+  layout: 'default',
+  name: 'IndexPage',
+}
+</script>
